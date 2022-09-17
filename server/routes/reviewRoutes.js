0d7023fe408680827e0b6c9b882a2c06c6461589
chí kiệt <lamchikiet.dev@gmail.com>
2022-09-17 11:46:04 +0700

    first commit

---
+++ server/routes/reviewRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+import reviewController from '../controllers/reviewControllers.js'
+
+//router.get()
+//router.put()
+//router.post()
+
+export default router
