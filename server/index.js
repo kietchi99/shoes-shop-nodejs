431b38c9da261e4e42ac73a296eb2791f04bc5cb
chí kiệt <lamchikiet.dev@gmail.com>
2022-09-17 11:49:57 +0700

    update

--- server/index.js
+++ server/index.js
@@ -1,37 +1,34 @@
-import express from 'express'
 import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
 import path from 'path'
 import mongooes from 'mongoose'
 import cookieParser from 'cookie-parser'
 import session from 'express-session'
 import methodOverride from 'method-override'
 import connectDB from './config/db.js'
-import route from './routes/routeIndex.js'
+import route from './routes/index.js'
 
 
 // init app
 const app = express()
-
+//connect to databases
+connectDB()
 //middlewares
 app.use(express.urlencoded())
 app.use(methodOverride('_method'))
 app.use(express.json())
 app.use(cookieParser())
 app.use(session({
-    secret: process.env.SS_SECRET,
+    secret: 'one piece',
     saveUninitialized: true,
     resave: true 
 })) 
-
-dotenv.config()
 
 //static file
 app.use(express.static('public'))
 
 //init routes
 route(app)
-
-//connect to databases
-
 
 app.listen(process.env.PORT) //3000
