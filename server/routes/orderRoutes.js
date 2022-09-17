afcdbb36ed2aa2d93484fee7863bfaf2075e6d8f
chí kiệt <lamchikiet.dev@gmail.com>
2022-09-17 11:47:46 +0700

    first commit

---
+++ server/routes/orderRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+import orderController from '../controllers/orderControllers.js'
+
+//router.get()
+//router.put()
+//router.post()
+
+export default router
---
+++ server/routes/productRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+import productController from '../controllers/productControllers.js'
+
+//router.get()
+//router.put()
+//router.post()
+
+export default router
