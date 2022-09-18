01261d93ee22dee19044dec9915069d9c22d35f9
chí kiệt <lamchikiet.dev@gmail.com>
2022-09-18 20:50:33 +0700

    add route

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -1,9 +1,13 @@
-const express = require('express')
+import express from 'express'
 const router = express.Router()
 import reviewController from '../controllers/reviewControllers.js'
 
-//router.get()
-//router.put()
-//router.post()
+router.get('', reviewController.getAllReviews)
+router.put('/:id/update', reviewController.updateReview)
+router.post('/:id/add', reviewController.addReview)
+router.delete('/:id/remove', reviewController.removeReview)
+router.get('/:status/getbystatus', reviewController.getReviewByStatus)
+
+
 
 export default router
