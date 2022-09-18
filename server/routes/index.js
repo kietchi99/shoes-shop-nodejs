358c82c9a8ad825a8bb275922bf623a6ec5ff883
chí kiệt <lamchikiet.dev@gmail.com>
2022-09-18 20:47:54 +0700

    add route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,14 @@
 import userRoutes from './userRoutes.js'
 //import orderRoutes from './routes/orderRoutes.js'
-//import productRoutes from './routes/productRoutes.js'
-//import reviewRoutes from './routes/reviewRoutes.js'
+import productRoutes from './productRoutes.js'
+import reviewRoutes from './reviewRoutes.js'
 
 function routes(app) {
 
     app.use('/api/users', userRoutes)
-    //app.use('/api/products')
+    app.use('/api/products', productRoutes)
+    app.use('/api/reviews', reviewRoutes)
     //app.use('/api/orders')
-    //app.use('/api/reviews')
+    
 }
 export default routes
