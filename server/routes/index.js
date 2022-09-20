72d6ae54bc6d1d48c658dd9fefc8898315d155f0
chí kiệt <lamchikiet.dev@gmail.com>
2022-09-20 09:56:43 +0700

    add order route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
 import userRoutes from './userRoutes.js'
-//import orderRoutes from './routes/orderRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 import productRoutes from './productRoutes.js'
 import reviewRoutes from './reviewRoutes.js'
 
@@ -8,7 +8,7 @@ function routes(app) {
     app.use('/api/users', userRoutes)
     app.use('/api/products', productRoutes)
     app.use('/api/reviews', reviewRoutes)
-    //app.use('/api/orders')
+    app.use('/api/orders', orderRoutes)
     
 }
 export default routes
