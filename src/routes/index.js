e2b664e31294be1b1928a593eaa140b6aa3580e1
chí kiệt <lamchikiet.dev@gmail.com>
2022-10-22 13:07:48 +0700

    update routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,9 @@ import productRoutes from './productRoutes.js'
 import reviewRoutes from './reviewRoutes.js'
 
 function routes(app) {
-    app.use('/api/users', userRoutes)
-    app.use('/api/products', productRoutes)
-    app.use('/api/reviews', reviewRoutes)
-    app.use('/api/orders', orderRoutes)
+    app.use('/api/v1/users', userRoutes)
+    app.use('/api/v1/products', productRoutes)
+    app.use('/api/v1/reviews', reviewRoutes)
+    app.use('/api/v1/orders', orderRoutes)
 }
 export default routes
