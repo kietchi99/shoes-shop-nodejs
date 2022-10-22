42be901f9b538d995654b6058b19cdf99be1d075
chí kiệt <lamchikiet.dev@gmail.com>
2022-10-22 13:00:20 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,45 +1,49 @@
 import dotenv from 'dotenv'
-dotenv.config()
 import express from 'express'
+import morgan from 'morgan';
 import cors from 'cors'
 import path from 'path'
 import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename); 
-import mongooes from 'mongoose'
-import cookieParser from 'cookie-parser'
-import session from 'express-session'
+import mongoSanitize from 'express-mongo-sanitize';
+import xss from 'xss-clean'
 import methodOverride from 'method-override'
 import connectDB from './config/db.js'
-import route from './routes/index.js'
-import { engine } from 'express-handlebars'
+import routes from './routes/index.js'
+import AppError from './utils/appError.js'
+import globalErrorHandler from './controllers/errorControllers.js'
 
+//config .env
+dotenv.config()
 
 // init app
 const app = express()
+
 //connect to databases
 connectDB()
+
 //middlewares
 app.use(cors())
 app.use(express.urlencoded())
 app.use(methodOverride('_method'))
 app.use(express.json())
-app.use(cookieParser())
-app.use(session({
-    secret: 'one piece',
-    saveUninitialized: true,
-    resave: true 
-})) 
-
-
-app.engine('.hbs', engine({extname: '.hbs'}))
-app.set('view engine', '.hbs')
-app.set('views', path.join(__dirname, 'views'))
+app.use(mongoSanitize())
+app.use(xss())
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
 //static file
 app.use("/public", express.static(__dirname + '/public'));
 
 //init routes
-route(app)
+routes(app)
 
-app.listen(process.env.PORT) //3000
+//error middlewares
+app.all('*', (req, res, next)=>{
+    next(new AppError(`Trang '${req.originalUrl}' không tồn tại`, 404));
+});
+app.use(globalErrorHandler)
+
+app.listen(process.env.PORT) //5000
