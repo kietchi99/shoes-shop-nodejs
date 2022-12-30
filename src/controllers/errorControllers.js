2e4668550a76a3179e756e7a5378ae6fe83f3ba3
chí kiệt <lamchikiet.dev@gmail.com>
2022-12-30 15:22:28 +0700

    clean code

--- src/controllers/errorControllers.js
+++ src/controllers/errorControllers.js
@@ -1,5 +1,6 @@
 import AppError from '../utils/appError.js'
 
+// development environment
 const sendErrorDev = (err, res) =>{
     res.status(err.statusCode).json({
         status: err.status,
@@ -9,6 +10,7 @@ const sendErrorDev = (err, res) =>{
     })
 }
 
+//production environment
 const sendErrorProd = (err, res) =>{
     if(err.isOperational) {
         res.status(err.statusCode).json({
@@ -24,10 +26,13 @@ const sendErrorProd = (err, res) =>{
     }   
 }
 
+//Cast error
 const handleCastErrorDB = err => {
     const message = `Giá trị ${err.value} của ${err.path} không hợp lệ`
     return new AppError(message, 400)
 }
+
+//Duplicate error
 const handleDuplicateFieldsDB = err => {
     const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)
 
@@ -35,15 +40,20 @@ const handleDuplicateFieldsDB = err => {
     return new AppError(message, 400)
 }
 
+//Validation error
 const handleValidationErrorDB = err => {
     const error = Object.values(err.errors).map(val=>val.message);
     const message = `Giá trị nhập vào không hợp lệ. ${error.join('. ')}`
     return new AppError(message, 400)
 }
 
+//JWT Invalid 
 const handleJWTError = () => new AppError('Token không hợp lệ', 401)
+
+//JWT expired error
 const handleJWTExpiredError = () => new AppError('Token hết hạn', 401)
 
+//Global error
 const globalErrorHandler = (err, req, res, next)=>{
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
