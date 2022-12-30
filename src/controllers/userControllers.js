373552fb4a9e0793523c98322b5824858482854d
chí kiệt <lamchikiet.dev@gmail.com>
2022-12-30 15:53:05 +0700

    update

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,156 +1,151 @@
 import User from '../models/user.js'
-import { handleDate, paginate, filterObj } from '../utils/util.js'
+import { handleDate, paginate, filterObj } from '../utils/index.js'
+import catchAsync from '../utils/catchAsync.js'
+import AppError from '../utils/appError.js'
 
-
-// get a user by id
+// Get a user by id
 // [GET] api/v1/users/:id
-export const getUser = async (req, res) => {
-    try{
-        const user = await User.findById(req.params.id).select('-password')
+// Private
+// Addmin
+export const getUser = catchAsync(async (req, res) => {
+    const user = await User.findById(req.params.id).select('-password')
 
-        if (!user) {
-            throw ('không tìm thấy người dùng')
+    if (!user) return next(new AppError('Không tìm thấy người dùng', 404))
+
+    res.status(200).json({
+        status: 'Success',
+        data: {
+            user
         }
-        res.status(200).json({
-            status: 'Success',
-            data: {
-                user
+    })
+})
+
+// Get all users
+// [GET] api/v1/users
+// Private
+// Admin
+export const getAllUsers = catchAsync(async (req, res) => {
+    let { keyword, page } = req.query
+    let tempQuery = []
+
+    //search
+    if (keyword){
+        tempQuery.push({
+            $match: {
+                $or: 
+                    [
+                        { fullName : { $regex: keyword, $options: 'i'}}, 
+                        { email : { $regex: keyword, $options: 'i' }}
+                    ] 
             }
         })
-    }catch(err){
-        res.status(500).json({status: 'error', message: err})
     }
-}
 
-// get all users
-// [GET] api/v1/users/
-export const getAllusers = async (req, res) => {
-    try {
-        let { keyword, page } = req.query
-        let tempQuery = []
-
-        //search
-        if (keyword){
-            tempQuery.push({
-                $match: {$or: [
-                    {fullName : { $regex: keyword, $options: 'i'}}, 
-                    {email : { $regex: keyword, $options: 'i' }}
-                ] }
-            })
+    const { query, totalPage, currentPage }  = await paginate(tempQuery, User, page)
+    
+    const users = await User.aggregate(query)
+    res.status(200).json({  
+        status: 'Success',
+        data: {
+            users,
+            meta: { 
+                totalPage,  
+                currentPage
+            } 
         }
+    })
+})
 
-        const { query, totalPage, currentPage }  = await paginate(tempQuery, User, page)
-        
-        const users = await User.aggregate(query)
-        res.status(200).json({  
-            status: 'Success',
-            data: {
-                users,
-                meta: { 
-                    totalPage,  
-                    currentPage
-                } 
-            }
-        })
-    }catch(err){
-        console.log(err)
-        res.status(400).json({status: 'Error', message: err })
-    }
-}
-
-// update profile
+// Update profile
 // [PATCH] api/v1/users/updateProfile
-export const updateProfile = async (req, res) => {
-    try{
-        const filteredBody = filterObj(req.body, 'fullName', 'email')
-        if(req.file) {
-            filteredBody.photo = req.file.filename
+// Private
+// Customer
+export const updateMyProfile = catchAsync(async (req, res) => {
+    const filteredBody = filterObj(req.body, 'fullName', 'email')
+
+    if(req.file) filteredBody.photo = req.file.filename
+
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+        new: true,
+        runValidators: true
+    })
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: updatedUser
         }
-        filteredBody.photo = '' ? undefined : filteredBody.photo
-        const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
-            new: true,
-            runValidators: true
-        })
-
-        res.status(200).json({
-            status: 'success',
-            data: {
-                user: updatedUser
-            }
-        })
-    }catch(err){
-        console.log(err)
-        res.status(500).json({status: 'Error', message: err})
-    }
-}
+    })
+})
 
 
-// update pasword 
+// Update pasword 
 // [PATCH] api/v1/users/updatepassword
-export const updateMyPassword = async (req, res) => {
-    try{  
-        const user = await User.findById(req.user.id).select('+password')
+// Private
+// Customer
+export const updateMyPassword = catchAsync(async (req, res) => {
+    const user = await User.findById(req.user.id).select('+password')
 
-        if(!(await user.correctPassword(req.body.passwordConfirm, user.password))) {
-            throw('Mật khẩu không khớp')
-        }
-
-        user.password = req.body.password
-        user.passwordConfirm = req.body.passwordConfirm
-        await user.save()
-    }catch(err){
-        console.log(err)
-        res.status(500).json({status: 'Error', message: err})
+    if( await user.correctPassword(req.body.password, user.password) === false ) {
+        return next(new AppError('Mật khẩu không khớp', 400))
     }
-}
 
-//update user
+    user.password = req.body.password
+    user.passwordConfirm = req.body.passwordConfirm
+    await user.save()
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user
+        }
+    })
+})
+
+// Update user
 // [PATCH] api/v1/users/:id
-export const updateUser =  async (req, res) => {
-    try{
-        const filteredBody = filterObj(req.body, 'status', 'role')
+// Private
+// Admin
+export const updateUser =  catchAsync(async(req, res) => {
+    const filteredBody = filterObj(req.body, 'status', 'role')
 
-        const updatedUser = await User.findByIdAndUpdate(req.params.id, filteredBody, {
-            new: true,
-            runValidators: true
-        })
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, filteredBody, {
+        new: true,
+        runValidators: true
+    })
 
-        if(!updatedUser) {
-            throw ('không tìm thấy người dùng')
+    if (!updatedUser) return next(new AppError('Không tìm thấy người dùng', 404))
+
+    res.status(200).json({
+        status: 'Success',
+        data: {
+            updatedUser
         }
+    })
+})
 
-        res.status(200).json({
-            status: 'Success',
-            data: {
-                updatedUser
-            }
-        })
-    }catch(err){
-        res.status(500).json({status: 'Error', message: err})
-    }
-}
-
-//total user
+// Total user
 // [GET] api/v1/users/total
-export const totalUsers = async (req, res) => {
-    try{
-        let totalUsers = 0
-        if(req.query.month && req.query.year) {
-            const {start, end} = handleDate(req.query.month, req.query.year)
-            totalUsers = await User.aggregate([
-                {$match: {createdAt: {$gte: start, $lte: end } }}, 
-                {
-                    $group: {_id: {$dateToString: {"date": "$createdAt","format": "%Y-%m"}},
-                    Count: {$sum: 1}}
-                }
-            ])
-            totalUsers = totalUsers.length === 0 ? 0 : totalUsers[0].Count
-        }else{ 
-            totalUsers = await User.countDocuments({})
-        }
-        res.status(200).json({status: 'Success', message: 'Lấy dữ liệu thành công', data: totalUsers})
-    }catch(err){
-        console.log(err)
-        res.status(500).json({status: 'Error', message: err})
+// Private
+// Admin
+export const totalUsers = catchAsync(async (req, res) => {
+    let totalUsers = 0
+    if(req.query.month && req.query.year) {
+        const {start, end} = handleDate(req.query.month, req.query.year)
+        totalUsers = await User.aggregate([
+            {$match: {createdAt: {$gte: start, $lte: end } }}, 
+            {
+                $group: {_id: {$dateToString: {"date": "$createdAt","format": "%Y-%m"}},
+                Count: {$sum: 1}}
+            }
+        ])
+        totalUsers = totalUsers.length === 0 ? 0 : totalUsers[0].Count
+    }else{ 
+        totalUsers = await User.countDocuments({})
     }
-}
+    res.status(200).json({
+        status: 'Success', 
+        data: {
+            totalUsers
+        }
+    })
+})
