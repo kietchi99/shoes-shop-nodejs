1affb65dfd999ca5af45e3752f17bcc3c2e8d08b
chí kiệt <lamchikiet.dev@gmail.com>
2022-12-30 16:25:22 +0700

    clean code

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,14 +1,43 @@
 import express from 'express'
-const router = express.Router()
-import orderController from '../controllers/orderControllers.js'
+import { protect, restrictTo } from '../controllers/authControllers.js'
+import {
+    addOrder,
+    updateOrder,
+    getOrder,
+    getOrders
+} from '../controllers/orderControllers.js'
 
-router.post('/add',orderController.addOrder)
-router.put('/:id/:status/update',orderController.updateOrder)
-router.get('/total',orderController.totalOrders)
-router.get('/totalsaleamount',orderController.totalSaleAmount)
-router.get('/:id/getbyid',orderController.getOrderById)
-router.get('', orderController.getAllOrders)
-router.get('/:status/getbystatus', orderController.getOrdersByStatus)
-router.get('/getbyiduser', orderController.getOrdersByIdUser)
+const router = express.Router()
+
+// Add a order
+// Private
+// Cutomer
+router.post('/', restrictTo("customer"), addOrder)
+
+// Update order
+// Private
+// Admin && customer
+router.patch('/:id', protect,  restrictTo("customer", "admin"), updateOrder)
+
+// Order details
+// Private
+// Admin && customer
+router.get('/:id', protect,  restrictTo("customer", "admin"), getOrder)
+
+// Get orders by customer id
+// Private
+// Customer
+router.get('/users/:id', protect,  restrictTo("customer"), getOrders)
+
+//All orders
+// Private
+//Admin
+router.get('/', protect,  restrictTo("admin"), getOrders)
+
+
+//Total orders
+// Private
+//Admin
+
 
 export default router
