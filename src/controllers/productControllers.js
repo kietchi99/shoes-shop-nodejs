2626f59536330c5655c8f6120b951ae9d616a6ae
chí kiệt <lamchikiet.dev@gmail.com>
2022-12-30 15:46:24 +0700

    add update photos feature

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -1,3 +1,5 @@
+import multer from 'multer'
+import sharp from 'sharp'
 import catchAsync from '../utils/catchAsync.js'
 import Product from '../models/product.js'
 import { 
@@ -6,6 +8,57 @@ import {
     handleInputProduct,
     filterObj
 } from '../utils/index.js'
+
+//setup multer - upload images
+const multerStorage = multer.memoryStorage()
+
+const multerFilter = (req, file, cb) => {
+    if(file.mimetype.startsWith('image')) {
+        cb(null, true)
+    }else {
+        cb(new AppError('Không phải hình ảnh', 400), false)
+    }
+}
+
+const upload = multer({
+    storage: multerStorage, 
+    fileFilter: multerFilter
+})
+
+export const uploadProductImages = upload.fields([
+    { name: 'imageCover', maxCount: 1 },
+    { name: 'images', maxCount: 6 }
+])
+
+// resize image
+export const resizeProductImages = catchAsync(async(req, res, next) => {
+    if(!req.files.imageCover && !req.files.images) return next()
+
+    if(req.files.imageCover){
+        req.body.imageCover = `product-${req.params.id}-${Date.now()}-cover.jpeg`
+        await sharp(req.files.imageCover[0].buffer)
+            .resize(2000, 1333)
+            .toFormat('jpeg')
+            .jpeg({ quality: 90 })
+            .toFile(`src/public/img/products/${req.body.imageCover}`)
+    }
+
+    if (req.files.images) {
+        req.body.images = []
+        await Promise.all(
+            req.files.images.map(async(file, i)=>{
+                const filename = `product-${req.params.id}-${Date.now()}-${i+0}.jpeg`
+                await sharp(file.buffer)
+                    .resize(2000, 1333)
+                    .toFormat('jpeg')
+                    .jpeg({ quality: 90 })
+                    .toFile(`src/public/img/products/${filename}`)
+                req.body.images.push(filename)
+            })
+        )
+    }
+    next()
+})
 
 // Get product by sku
 // [GET] api/v1/products/sku/:sku
