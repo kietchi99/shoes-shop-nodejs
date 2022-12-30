605e2d95b578d5544669f9ad1f1eeea6f77e7c6c
chí kiệt <lamchikiet.dev@gmail.com>
2022-12-30 15:54:26 +0700

    add update photos feature

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,7 +1,41 @@
+import multer from 'multer'
+import sharp from 'sharp'
 import User from '../models/user.js'
 import { handleDate, paginate, filterObj } from '../utils/index.js'
 import catchAsync from '../utils/catchAsync.js'
 import AppError from '../utils/appError.js'
+
+
+//setup multer - upload images
+const multerStorage = multer.memoryStorage()
+
+const multerFilter = (req, file, cb) => {
+    if(file.mimetype.startsWith('image')) {
+        cb(null, true)
+    }else {
+        cb(new AppError('Không phải hình ảnh', 400), false)
+    }
+}
+
+const upload = multer({
+    storage: multerStorage, 
+    fileFilter: multerFilter
+})
+
+export const uploadUserPhoto = upload.single('photo')
+export const resizeUserPhoto = catchAsync(async(req, res, next) => {
+    if(!req.file) return next()
+
+    req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`
+    await sharp(req.file.buffer)
+        .resize(500, 500)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`src/public/img/users/${req.file.filename}`)
+
+    next()
+})
+
 
 // Get a user by id
 // [GET] api/v1/users/:id
